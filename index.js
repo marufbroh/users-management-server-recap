b1ed27baa0ba4daef15cd936769fa64de9d15a80
marufbroh <marufmd775@gmail.com>
2023-10-10 12:19:16 +0600

    user post done

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const users = [
-    { id: 1, name: 'Sabana', email: 'sabana@gmail.com' },
-    { id: 2, name: 'Sabnoon', email: 'sabnoon@gmail.com' },
-    { id: 3, name: 'Sabila', email: 'sabila@gmail.com' },
-]
+  { id: 1, name: "Sabana", email: "sabana@gmail.com" },
+  { id: 2, name: "Sabnoon", email: "sabnoon@gmail.com" },
+  { id: 3, name: "Sabila", email: "sabila@gmail.com" },
+];
 
 app.get("/", (req, res) => {
-    res.send("Server is running")
-})
+  res.send("Server is running");
+});
 
 app.get("/users", (req, res) => {
-    res.send(users)
-})
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  // console.log(newUser)
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
